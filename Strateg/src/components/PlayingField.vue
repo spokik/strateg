f2683430f7eb3dd96a629634b85e8a33d28e53b0
Lyalin.s <lyalin.s@pfm.team>
2022-04-17 14:52:28 +0300

    Проверка победителя по горизонтали

--- Strateg/src/components/PlayingField.vue
+++ Strateg/src/components/PlayingField.vue
@@ -6,20 +6,56 @@ import Cell from "./Cell.vue";
 const props = defineProps<{ pool: Array<Array<ICell>>; player: string }>();
 const emit = defineEmits(["playerChange"]);
 
+const isAllSameColor = (el0, el1, el2, el3) => {
+  if ([el0, el1, el2, el3].includes("white")) {
+    return false;
+  } else {
+    if ([el0, el1, el2, el3].every((el) => el === "red")) {
+      return el0;
+    } else if ([el0, el1, el2, el3].every((el) => el === "yellow")) {
+      return el0;
+    }
+  }
+};
 const gamePool = ref(props.pool);
+const horizontal = (gamePool) => {
+  for (let i = 0; i <= 5; i++) {
+    for (let x = 0; x < 4; x++) {
+      let result = isAllSameColor(
+        gamePool[i][x].color,
+        gamePool[i][x + 1].color,
+        gamePool[i][x + 2].color,
+        gamePool[i][x + 3].color
+      );
+      if (result) {
+        return result;
+      }
+    }
+  }
+};
+const vertical = (gamePool) => {};
+const diagonally = (gamePool) => {};
+const checkPool = (gamePool) => {
+  if (horizontal(gamePool) || diagonally(gamePool) || vertical(gamePool)) {
+    alert(`победил ${horizontal(gamePool)}`);
+  }
+};
 
 function setColor(event: ICell) {
   let i = 5;
   let stoped = true;
+  //Ставим шарик в нюжнюю ячейку
   while (i >= 0 && stoped) {
     if (gamePool.value[i][event.x].color === "white") {
-      console.log("123");
       gamePool.value[i][event.x].color = props.player;
       stoped = false;
       emit("playerChange");
     }
     i--;
   }
+
+  //Проерка поля на наличие выигрешных комбинаций
+  checkPool(gamePool.value);
 }
 
 const count = ref(0);
