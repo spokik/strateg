07563eb10dbcb23b3bd6f655960bbb72dd0ba365
Lyalin.s <lyalin.s@pfm.team>
2022-05-03 10:37:12 +0300

    исправена diagonallyY

--- Strateg/src/components/PlayingField.vue
+++ Strateg/src/components/PlayingField.vue
@@ -119,7 +119,7 @@ const diagonallyY = (gamePool: Pool) => {
             [x, y],
             [x + 1, y + 1],
             [x + 2, y + 2],
-            [x + 4, y + 3],
+            [x + 3, y + 3],
           ],
         ];
       }
